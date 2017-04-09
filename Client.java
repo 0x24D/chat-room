005704107f3bceaba6a2f6608a2451abf0b06740
0x24d <daniel.longfield@tuta.io>
2017-04-09 23:56:22 +0100

    Username/password changing implemeted - need to update user in chat, audio/video started (NPE)

--- Client.java
+++ Client.java
@@ -10,300 +10,297 @@ import javax.media.*;
 
 public class Client extends JFrame implements ControllerListener
 {
-	private static JTextArea outputField;
-	private static JTextField inputField, userField;
-	private static JList<String> userList;
-	private static Vector<String> users;
-	private static JPanel detailsPanel;
-	private static JPasswordField passwordField;
-	private static ImageIcon image;
-	private static JDialog dialog;
-	private static Player player;
+    private static JTextArea outputField;
+    private static JTextField inputField, userField;
+    private static JList<String> userList;
+    private static Vector<String> users;
+    private static JPanel detailsPanel;
+    private static JPasswordField passwordField;
+    private static ImageIcon image;
+    private static JDialog dialog;
+    private static Player player;
 
-	public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException
+    {
+	InetAddress host = null;
+	final int PORT = 1234;
+	Socket socket;
+	Scanner keyboard, networkInput;
+	PrintWriter output;
+	MessageThread thread;
+	Client frame;
+	ObjectInputStream fileIn;
+
+	try
 	{
-		InetAddress host = null;
-		final int PORT = 1234;
-		Socket socket;
-		Scanner keyboard, networkInput;
-		PrintWriter output;
-		MessageThread thread;
-		Client frame;
-		ObjectInputStream fileIn;
+	    host = InetAddress.getLocalHost();
+	} catch (UnknownHostException uhEx)
+	{
+	    outputField.append("\nHost ID not found!\n");
+	}
 
+	socket = new Socket(host, PORT);
+	output = new PrintWriter(socket.getOutputStream(), true);
+	keyboard = new Scanner(System.in);
+	networkInput = new Scanner(socket.getInputStream());
+	fileIn = new ObjectInputStream(socket.getInputStream());
+	thread = new MessageThread(networkInput);
+	frame = new Client(output);
+
+	frame.setTitle("Chat Client");
+	frame.setSize(800, 650);
+	frame.setVisible(true);
+	frame.pack();
+	thread.start();
+	JOptionPane.showConfirmDialog(frame, detailsPanel, "Login", JOptionPane.OK_CANCEL_OPTION);
+
+	output.println(userField.getText());
+	output.println(passwordField.getPassword());
+	String message = networkInput.nextLine();
+
+	while (!message.equals("UserQuit"))
+	{
+	    if (message.equals("CUStart"))
+	    {
+		message = networkInput.nextLine();
+		users.clear();
+		while (!message.equals("CUEnd"))
+		{
+		    users.add(message);
+		    message = networkInput.nextLine();
+		}
+		userList.setListData(users);
+	    } else if (message.substring(0, 8).equals("FileOpen"))
+	    {
 		try
-		{
-			host = InetAddress.getLocalHost();
-		}
-		catch(UnknownHostException uhEx)
-		{
-			outputField.append("\nHost ID not found!\n");
-		}
+		{ // download file
+		    byte[] byteArray = (byte[]) fileIn.readObject();
+		    FileOutputStream fileOutput = new FileOutputStream(message.substring(9));
+		    fileOutput.write(byteArray);
 
-		socket = new Socket(host, PORT);
-		output = new PrintWriter(socket.getOutputStream(),true);
-		keyboard = new Scanner(System.in);
-		networkInput = new Scanner(socket.getInputStream());
-		fileIn = new ObjectInputStream(socket.getInputStream());
-		thread = new MessageThread(networkInput);
-		frame = new Client(output);
+		    // get local file details
+		    File file = new File(message.substring(9));
+		    FileInputStream localFile = new FileInputStream(file);
+		    String mimeType = (Files.probeContentType(file.toPath())).substring(0, 5);
+		    // TODO: nullpointerexception - add File.exists()
+		    // open local file
+		    dialog = new JDialog();
+		    byteArray = new byte[(int) file.length()];
+		    localFile.read(byteArray);
+		    localFile.close();
 
-		frame.setTitle("Chat Client");
-		frame.setSize(800,650);
-		frame.setVisible(true);
-		frame.pack();
-		thread.start();
-		JOptionPane.showConfirmDialog(frame, detailsPanel, "Login", JOptionPane.OK_CANCEL_OPTION);
-
-		output.println(userField.getText());
-		output.println(passwordField.getPassword());
-		String message = networkInput.nextLine();
-
-		while (!message.equals("UserQuit"))
-		{
-			if (message.equals("CUStart"))
+		    if (mimeType.equals("image")) // display image
+		    {
+			ImageIcon image = new ImageIcon(byteArray);
+			JLabel label = new JLabel(image);
+			dialog.add(label);
+		    } else // display audio/video
+		    {
+			URI uri = file.toURI();
+			try
 			{
-				message = networkInput.nextLine();
-				users.clear();
-				while (!message.equals("CUEnd"))
-				{
-					users.add(message);
-					message = networkInput.nextLine();
-				}
-				userList.setListData(users);
-			}
-			else if(message.substring(0,8).equals("FileOpen"))
+			    player = Manager.createPlayer(uri.toURL());
+			} catch (NoPlayerException e)
 			{
-				try
-				{	//download file
-					byte[] byteArray = (byte[])fileIn.readObject();
-	      			FileOutputStream fileOutput = new FileOutputStream(message.substring(9));
-					fileOutput.write(byteArray);
-
-					//get local file details
-					File file = new File(message.substring(9));
-					FileInputStream localFile = new FileInputStream(file);
-					String mimeType = (Files.probeContentType(file.toPath())).substring(0,5);
-
-					//open local file
-					dialog = new JDialog();
-					byte[] fileByteArray = new byte[(int) file.length()];
-					localFile.read(fileByteArray);
-					localFile.close();
-
-					if(mimeType.equals("image")) //display image
-					{
-						ImageIcon image = new ImageIcon(fileByteArray);
-						JLabel label = new JLabel(image);
-						dialog.add(label);
-					}
-					else //display audio/video
-					{
-						URI uri = file.toURI();
-						try
-						{
-							player = Manager.createPlayer(uri.toURL());
-						}
-						catch(NoPlayerException e)
-						{
-							e.printStackTrace();
-						}
-						player.addControllerListener(frame);
-						player.start();
-					}
-					dialog.pack();
-					dialog.setVisible(true);
-				}
-				catch(IOException e)
-				{
-					e.printStackTrace(); //change
-				}
-				catch(ClassNotFoundException e)
-				{
-					e.printStackTrace(); //change
-				}
+			    e.printStackTrace();
 			}
-			else
-				outputField.append(message + "\n");
-			message = networkInput.nextLine();
-		}
-
-		try
+			player.addControllerListener(frame);
+			player.start();
+		    }
+		    dialog.pack();
+		    dialog.setVisible(true);
+		} catch (IOException e)
 		{
-			outputField.append("\nClosing down connection...\n");
-			users.clear();
-			userList.setListData(users);
-			socket.close();
-			keyboard.close();
-		}
-		catch(IOException ioEx)
+		    e.printStackTrace(); // change
+		} catch (ClassNotFoundException e)
 		{
-			outputField.append("\n* Disconnection problem! *\n");
+		    e.printStackTrace(); // change
 		}
+	    } else
+		outputField.append(message + "\n");
+	    message = networkInput.nextLine();
 	}
 
-	public Client(PrintWriter output)
+	try
 	{
-		JPanel panel, leftPanel, rightPanel;
-		JPanel outputPanel, inputPanel, usersPanel, buttonsPanel;
-		JButton sendButton, quitButton;
-		JLabel usersLabel, userLabel, passwordLabel;
-		Listener listener;
-
-		users = new Vector<String>();
-		userList = new JList<String>();
-		panel = new JPanel();
-		leftPanel = new JPanel();
-		rightPanel = new JPanel();
-		detailsPanel = new JPanel();
-		outputPanel = new JPanel();
-		inputPanel = new JPanel();
-		usersPanel = new JPanel();
-		buttonsPanel = new JPanel();
-		outputField = new JTextArea(34, 45);
-		inputField = new JTextField(44);
-		userField = new JTextField(12);
-		passwordField = new JPasswordField(12);
-		usersLabel = new JLabel("Connected users:");
-		userLabel = new JLabel("Username: ");
-		passwordLabel = new JLabel("Password: ");
-		sendButton = new JButton("Send message.");
-		quitButton = new JButton("Quit.");
-		listener = new Listener(output);
-
-		outputField.setWrapStyleWord(true);
-		outputField.setLineWrap(true);
-
-		outputField.setEditable(false);
-		inputField.setEditable(true);
-		userField.setEditable(true);
-		passwordField.setEditable(true);
-
-		outputField.setVisible(true);
-		inputField.setVisible(true);
-		userList.setVisible(true);
-
-		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
-		detailsPanel.setLayout(new BoxLayout(detailsPanel, BoxLayout.Y_AXIS));
-		inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
-		outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
-		usersPanel.setLayout(new BoxLayout(usersPanel, BoxLayout.Y_AXIS));
-		buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
-
-		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		outputPanel.setBorder(new EmptyBorder(16, 0, 10, 10));
-		inputPanel.setBorder(new EmptyBorder(0, 0, 0, 10));
-		usersPanel.setBorder(new EmptyBorder(0, 0, 10, 0));
-		detailsPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-
-		add(panel);
-		panel.add(leftPanel);
-		panel.add(rightPanel);
-		leftPanel.add(outputPanel);
-		leftPanel.add(inputPanel);
-		rightPanel.add(usersPanel);
-		rightPanel.add(buttonsPanel);
-		outputPanel.add(new JScrollPane(outputField));
-		inputPanel.add(new JScrollPane(inputField));
-		usersPanel.add(usersLabel);
-		usersPanel.add(new JScrollPane(userList));
-		buttonsPanel.add(sendButton);
-		buttonsPanel.add(quitButton);
-		detailsPanel.add(userLabel);
-		detailsPanel.add(userField);
-		detailsPanel.add(passwordLabel);
-		detailsPanel.add(passwordField);
-
-		usersLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		sendButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-		sendButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e)
-			{
-				output.println(inputField.getText());
-				inputField.setText("");
-			}
-		});
-		quitButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e)
-			{
-				output.println("/quit");
-			}
-		});
-		inputField.addKeyListener(new KeyListener(){
-			@Override
-			public void keyReleased(KeyEvent e)
-			{
-				if (e.getKeyCode() == KeyEvent.VK_ENTER)
-				{
-					output.println(inputField.getText());
-					inputField.setText("");
-				}
-			}
-			@Override
-			public void keyPressed(KeyEvent e)
-			{
-			}
-
-			@Override
-			public void keyTyped(KeyEvent e)
-			{
-			}
-		});
-		addWindowListener(listener);
-	}
-
-	public void controllerUpdate(ControllerEvent e)
+	    outputField.append("\nClosing down connection...\n");
+	    users.clear();
+	    userList.setListData(users);
+	    socket.close();
+	    keyboard.close();
+	} catch (IOException ioEx)
 	{
-		Container pane = dialog.getContentPane();
-
-		if(e instanceof RealizeCompleteEvent)
-		{
-			Component visualComponent = player.getVisualComponent();
-			if(visualComponent != null)
-				pane.add(visualComponent);
-
-			Component controlsComponent = player.getControlPanelComponent();
-			if(controlsComponent != null)
-				pane.add(controlsComponent);
-			pane.doLayout();
-		}
+	    outputField.append("\n* Disconnection problem! *\n");
 	}
+    }
 
-	class Listener extends WindowAdapter
+    public Client(PrintWriter output)
+    {
+	JPanel panel, leftPanel, rightPanel;
+	JPanel outputPanel, inputPanel, usersPanel, buttonsPanel;
+	JButton sendButton, quitButton;
+	JLabel usersLabel, userLabel, passwordLabel;
+	Listener listener;
+
+	users = new Vector<String>();
+	userList = new JList<String>();
+	panel = new JPanel();
+	leftPanel = new JPanel();
+	rightPanel = new JPanel();
+	detailsPanel = new JPanel();
+	outputPanel = new JPanel();
+	inputPanel = new JPanel();
+	usersPanel = new JPanel(); // change width
+	buttonsPanel = new JPanel();
+	outputField = new JTextArea(34, 45);
+	inputField = new JTextField(44);
+	userField = new JTextField(12);
+	passwordField = new JPasswordField(12);
+	usersLabel = new JLabel("Connected users:");
+	userLabel = new JLabel("Username: ");
+	passwordLabel = new JLabel("Password: ");
+	sendButton = new JButton("Send message.");
+	quitButton = new JButton("Quit.");
+	listener = new Listener(output);
+
+	outputField.setWrapStyleWord(true);
+	outputField.setLineWrap(true);
+
+	outputField.setEditable(false);
+	inputField.setEditable(true);
+	userField.setEditable(true);
+	passwordField.setEditable(true);
+
+	outputField.setVisible(true);
+	inputField.setVisible(true);
+	userList.setVisible(true);
+
+	panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+	leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+	rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+	detailsPanel.setLayout(new BoxLayout(detailsPanel, BoxLayout.Y_AXIS));
+	inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+	outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
+	usersPanel.setLayout(new BoxLayout(usersPanel, BoxLayout.Y_AXIS));
+	buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+
+	panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+	outputPanel.setBorder(new EmptyBorder(16, 0, 10, 10));
+	inputPanel.setBorder(new EmptyBorder(0, 0, 0, 10));
+	usersPanel.setBorder(new EmptyBorder(0, 0, 10, 0));
+	detailsPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+	add(panel);
+	panel.add(leftPanel);
+	panel.add(rightPanel);
+	leftPanel.add(outputPanel);
+	leftPanel.add(inputPanel);
+	rightPanel.add(usersPanel);
+	rightPanel.add(buttonsPanel);
+	outputPanel.add(new JScrollPane(outputField));
+	inputPanel.add(new JScrollPane(inputField));
+	usersPanel.add(usersLabel);
+	usersPanel.add(new JScrollPane(userList));
+	buttonsPanel.add(sendButton);
+	buttonsPanel.add(quitButton);
+	detailsPanel.add(userLabel);
+	detailsPanel.add(userField);
+	detailsPanel.add(passwordLabel);
+	detailsPanel.add(passwordField);
+
+	usersLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+	sendButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+	quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+	sendButton.addActionListener(new ActionListener()
 	{
-		PrintWriter output;
-		public Listener(PrintWriter output)
+	    public void actionPerformed(ActionEvent e)
+	    {
+		output.println(inputField.getText());
+		inputField.setText("");
+	    }
+	});
+	quitButton.addActionListener(new ActionListener()
+	{
+	    public void actionPerformed(ActionEvent e)
+	    {
+		output.println("/quit");
+	    }
+	});
+	inputField.addKeyListener(new KeyListener()
+	{
+	    @Override
+	    public void keyReleased(KeyEvent e)
+	    {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER)
 		{
-			this.output = output;
+		    output.println(inputField.getText());
+		    inputField.setText("");
 		}
+	    }
 
-		public void windowClosing(WindowEvent e)
-		{
-			output.println("/quit");
-			System.exit(0);
-		}
+	    @Override
+	    public void keyPressed(KeyEvent e)
+	    {
+	    }
+
+	    @Override
+	    public void keyTyped(KeyEvent e)
+	    {
+	    }
+	});
+	addWindowListener(listener);
+    }
+
+    public void controllerUpdate(ControllerEvent e)
+    {
+	Container pane = dialog.getContentPane();
+
+	if (e instanceof RealizeCompleteEvent)
+	{
+	    Component visualComponent = player.getVisualComponent();
+	    if (visualComponent != null)
+		pane.add(visualComponent);
+
+	    Component controlsComponent = player.getControlPanelComponent();
+	    if (controlsComponent != null)
+		pane.add(controlsComponent);
+	    pane.doLayout();
 	}
+    }
+
+    class Listener extends WindowAdapter
+    {
+	PrintWriter output;
+
+	public Listener(PrintWriter output)
+	{
+	    this.output = output;
+	}
+
+	public void windowClosing(WindowEvent e)
+	{
+	    output.println("/quit");
+	    System.exit(0);
+	}
+    }
 }
 
 class MessageThread extends Thread
 {
 
-	public MessageThread(Scanner networkInput)
-	{
-	}
+    public MessageThread(Scanner networkInput)
+    {
+    }
 
-	public void run(JTextArea outputField, Scanner networkInput)
+    public void run(JTextArea outputField, Scanner networkInput)
+    {
+	String message = networkInput.nextLine();
+	while (message != "UserQuit")
 	{
-		String message = networkInput.nextLine();
-		while (message != "UserQuit")
-		{
-			outputField.append(message);
-			message = networkInput.nextLine();
-		}
+	    outputField.append(message);
+	    message = networkInput.nextLine();
 	}
+    }
 
 }
--- Server.java
+++ Server.java
@@ -6,219 +6,243 @@ import javax.swing.*;
 
 public class Server
 {
-	public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException
+    {
+	ServerSocket serverSocket = null;
+	final int PORT = 1234;
+	Socket client;
+	ClientHandler handler;
+	Connection connection = null;
+
+	try
 	{
-		ServerSocket serverSocket = null;
-		final int PORT = 1234;
-		Socket client;
-		ClientHandler handler;
-		Connection connection = null;
-
-		try
-		{
-			serverSocket = new ServerSocket(PORT);
-		}
-		catch (IOException e)
-		{
-			System.out.println("\nUnable to set up port!");
-			System.exit(1);
-		}
-
-		try
-		{
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatroom","root","root");
-		}
-		catch(SQLException e)
-		{
-			System.out.println("\nCannot connect to database!\n");
-			System.exit(1);
-		}
-
-		System.out.println("\nServer running...\n");
-
-		do
-		{
-			//Wait for client.
-			client = serverSocket.accept();
-			System.out.println("\nNew client accepted.\n");
-			handler = new ClientHandler(client, connection);
-			handler.start();
-		}while (true);
+	    serverSocket = new ServerSocket(PORT);
+	} catch (IOException e)
+	{
+	    System.out.println("\nUnable to set up port!");
+	    System.exit(1);
 	}
+
+	try
+	{
+	    connection = DriverManager.getConnection("jdbc:mysql://192.168.0.2:3306/chatroom", "root", "root");
+	} catch (SQLException e)
+	{
+	    System.out.println("\nCannot connect to database!\n");
+	    System.exit(1);
+	}
+
+	System.out.println("\nServer running...\n");
+
+	do
+	{
+	    // Wait for client.
+	    client = serverSocket.accept();
+	    System.out.println("\nNew client accepted.\n");
+	    handler = new ClientHandler(client, connection);
+	    handler.start();
+	} while (true);
+    }
 }
 
 class ClientHandler extends Thread
 {
-	private Socket client;
-	private Scanner input;
-	private PrintWriter output;
-	private ObjectOutputStream fileOutput;
-	private User user;
-	private static ArrayList<User> userList = new ArrayList<>();
-	private Connection connection;
+    private Socket client;
+    private Scanner input;
+    private PrintWriter output;
+    private ObjectOutputStream fileOutput;
+    private User user;
+    private static ArrayList<User> userList = new ArrayList<>();
+    private Connection connection;
 
-	public ClientHandler(Socket socket, Connection connection) throws IOException
+    public ClientHandler(Socket socket, Connection connection) throws IOException
+    {
+	client = socket;
+	input = new Scanner(client.getInputStream());
+	output = new PrintWriter(client.getOutputStream(), true);
+	fileOutput = new ObjectOutputStream(client.getOutputStream());
+	this.connection = connection;
+    }
+
+    public void run()
+    {
+	boolean validLogin = false;
+	String username = input.nextLine();
+	String password = input.nextLine();
+	String query = "SELECT password FROM Users WHERE username = '" + username + "';";
+
+	try
 	{
-		client = socket;
-		input = new Scanner(client.getInputStream());
-		output = new PrintWriter(client.getOutputStream(), true);
-		fileOutput = new ObjectOutputStream(client.getOutputStream());
-		this.connection = connection;
+	    Statement statement = connection.createStatement();
+	    ResultSet results = statement.executeQuery(query);
+
+	    while (results.next() && !validLogin)
+	    {
+		if (results.getString("password").equals(password))
+		    validLogin = true;
+	    }
+	} catch (SQLException e)
+	{
+	    output.println("Unable to validate user, please try again later.");
 	}
 
-	public void run()
+	if (validLogin)
 	{
-		boolean validLogin = false;
-		String username = input.nextLine();
-		String password = input.nextLine();
-		String query = "SELECT password FROM Users WHERE username = '" + username + "';";
+	    user = new User(username, client);
+	    userList.add(user);
+	    System.out.println(user.getUsername() + " connected.");
+	    outputMessage(user.getUsername() + " has connected.");
+	    updateUserList();
+	    String received = input.nextLine();
 
-		try
+	    while (!received.equals("/quit"))
+	    {
+		if (received.substring(0, 1).equals("/"))
 		{
-			Statement statement = connection.createStatement();
-			ResultSet results = statement.executeQuery(query);
-
-			while(results.next() && !validLogin)
+		    if (received.length() >= 5 && received.substring(0, 5).equals("/open"))
+		    {
+			String fileName = received.substring(6);
+			File file = new File("media" + File.separator + fileName);
+			if (file.exists())
 			{
-				if (results.getString("password").equals(password))
-					validLogin = true;
-			}
-		}
-		catch(SQLException e)
-		{
-			output.println("Unable to validate user, please try again later.");
-		}
+			    output.println("FileOpen:" + fileName);
+			    try
+			    {
+				FileInputStream fileInput = new FileInputStream(file);
+				long fLength = file.length();
+				int intFLength = (int) fLength;
+				byte[] byteArray = new byte[intFLength];
 
-		if (validLogin)
-		{
-			user = new User(username, client);
-			userList.add(user);
-			System.out.println(user.getUsername() + " connected.");
-			outputMessage(user.getUsername() + " has connected.");
-			updateUserList();
-			String received = input.nextLine();
+				fileInput.read(byteArray);
+				fileInput.close();
+				fileOutput.writeObject(byteArray);
+				fileOutput.flush();
+			    } catch (IOException e)
+			    {
+				System.out.println(e);
+			    }
+			} else
+			    output.println("Requested file (" + fileName + ") does not exist.");
+		    } else if (received.length() >= 5 && received.substring(0, 5).equals("/name"))
+		    {
+			updateDatabase(connection, "Users", "username", received.substring(6));
+		    } else if (received.length() >= 9 && received.substring(0, 9).equals("/password"))
+		    {
+			updateDatabase(connection, "Users", "password", received.substring(10));
+		    } else
+			output.println("Unknown system command.");
+		} else
+		    outputMessage(user.getUsername() + "> " + received);
+		received = input.nextLine();
+	    }
+	} else
+	    output.println("Invalid user! Open a new client to try again.");
 
-			while (!received.equals("/quit"))
-			{
-				if(received.substring(0,1).equals("/"))
-				{
-					if(received.substring(0,5).equals("/open"))
-					{
-						String fileName = received.substring(6);
-						File file = new File("media" + File.separator + fileName);
-						if(file.exists())
-						{
-							output.println("FileOpen:" + fileName);
-							try
-							{
-								FileInputStream fileInput = new FileInputStream(file);
-								long fLength = file.length();
-								int intFLength = (int)fLength;
-								byte[] byteArray = new byte[intFLength];
-
-								fileInput.read(byteArray);
-								fileInput.close();
-								fileOutput.writeObject(byteArray);
-								fileOutput.flush();
-							}
-							catch (IOException e)
-							{
-								System.out.println(e);
-							}
-						}
-						else
-							output.println("Requested file (" + fileName + ") does not exist.");
-					}
-					else
-						output.println("Unknown system command.");
-				}
-				else
-					outputMessage(user.getUsername() + "> " + received);
-				received = input.nextLine();
-			}
-		}
-		else
-			output.println("Invalid user! Open a new client to try again.");
-
-		try
-		{
-			System.out.println("Closing down connection...");
-			output.println("UserQuit");
-			System.out.println(user.getUsername() + ", "
-								+ user.getSocket() + " disconnected.");
-			outputMessage(user.getUsername() + " has disconnected.");
-			input.close();
-			output.close();
-			client.close();
-			userList.remove(userList.indexOf(user));
-			updateUserList();
-
-		}
-		catch(IOException e)
-		{
-			System.out.println("* Disconnection problem! *");
-		}
-	}
-	public void updateUserList()
+	try
 	{
-		PrintWriter tempOutput;
-		try
-		{
-			for (User user : userList)
-			{
-				tempOutput = new PrintWriter(
-						(user.getSocket()).getOutputStream(), true);
-				tempOutput.println("CUStart");
-				for (User connectedUser : userList)
-				{
-					tempOutput.println(connectedUser.getUsername());
-				}
-				tempOutput.println("CUEnd");
-			}
-		}
-		catch(IOException e)
-		{
-			System.out.println(e);
-		}
-	}
+	    System.out.println("Closing down connection...");
+	    output.println("UserQuit");
+	    System.out.println(user.getUsername() + ", " + user.getSocket() + " disconnected.");
+	    outputMessage(user.getUsername() + " has disconnected.");
+	    input.close();
+	    output.close();
+	    client.close();
+	    userList.remove(userList.indexOf(user));
+	    updateUserList();
 
-	public void outputMessage(String message)
+	} catch (IOException e)
 	{
-		PrintWriter tempOutput;
-		try
-		{
-			for (User user : userList)
-			{
-				tempOutput = new PrintWriter(
-						(user.getSocket()).getOutputStream(), true);
-				tempOutput.println(message);
-			}
-		}
-		catch(IOException e)
-		{
-			System.out.println(e);
-		}
+	    System.out.println("* Disconnection problem! *");
 	}
+    }
+
+    public void updateUserList()
+    {
+	PrintWriter tempOutput;
+	try
+	{
+	    for (User user : userList)
+	    {
+		tempOutput = new PrintWriter((user.getSocket()).getOutputStream(), true);
+		tempOutput.println("CUStart");
+		for (User connectedUser : userList)
+		{
+		    tempOutput.println(connectedUser.getUsername());
+		}
+		tempOutput.println("CUEnd");
+	    }
+	} catch (IOException e)
+	{
+	    System.out.println(e);
+	}
+    }
+
+    public void outputMessage(String message)
+    {
+	PrintWriter tempOutput;
+	try
+	{
+	    for (User user : userList)
+	    {
+		tempOutput = new PrintWriter((user.getSocket()).getOutputStream(), true);
+		tempOutput.println(message);
+	    }
+	} catch (IOException e)
+	{
+	    System.out.println(e);
+	}
+    }
+
+    public void updateDatabase(Connection connection, String table, String field, String data)
+    {
+	int resultsChanged = 0;
+	String update = "UPDATE " + table + " SET " + field + " = '" + data + "' WHERE username = '" + user.getUsername()
+		+ "';";
+
+	try
+	{
+	    Statement statement = connection.createStatement();
+	    resultsChanged = statement.executeUpdate(update);
+	} catch (SQLException e)
+	{
+	    e.printStackTrace();
+	    output.println("Unable to update user details.");
+	}
+	if (resultsChanged >= 1)
+	{
+	    if (field.equals("username"))
+	    {
+		user.updateUsername(data);
+		output.println("Username has been changed.");
+	    } else
+		output.println("Password has been changed.");
+	}
+    }
 }
 
 class User
 {
-	private String username;
-	private Socket socket;
+    private String username;
+    private Socket socket;
 
-	public User(String username, Socket socket)
-	{
-		this.username = username;
-		this.socket = socket;
-	}
+    public User(String username, Socket socket)
+    {
+	this.username = username;
+	this.socket = socket;
+    }
 
-	public String getUsername()
-	{
-		return username;
-	}
+    public String getUsername()
+    {
+	return username;
+    }
 
-	public Socket getSocket()
-	{
-		return socket;
-	}
+    public Socket getSocket()
+    {
+	return socket;
+    }
+
+    public void updateUsername(String username)
+    {
+	this.username = username;
+    }
 }
